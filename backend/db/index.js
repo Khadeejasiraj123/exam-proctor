a340b79711e811e45a04de83a79faa8b412897c6
Khadeejasiraj123 <khadeejasiraj123@gmail.com>
2026-03-19 17:21:35 +0530

    fix database url for render

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,11 @@
-const pg = require("pg");
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
-
-const client = new pg.Client({
-  connectionString: connectionString || process.env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-console.log(`Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}`);
-client.connect();
-
-module.exports = client;
+module.exports = pool;
